95d57fce2fe2a7cb7d63b388cf18a89e682cde77
khorh <helen.khor@googlemail.com>
2021-03-29 16:08:46 +0100

    updated files

--- index.php
+++ index.php
@@ -7,8 +7,17 @@ require_once 'db-connection.php';
 function getProducts(PDO $db): array
 {
     $query = $db->prepare("SELECT `image`, `product_name`, 
-                                    `business_name`, `currency`, `price`,
+                                    `business_name`, `currency`, `price`
                                 FROM `products`;");
     $query->execute();
     return $query->fetchAll();
 }
+
+$db = getDb();
+$products = getProducts($db);
+
+echo '<ul>';
+foreach($products as $product) {
+    echo '<li>' . $product['image'] . $product['product_name'] . $product['business_name'] . $product['currency'] . $product['price'] . '</li>';
+}
+echo '</ul>';
